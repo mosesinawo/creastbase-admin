79ff8699e4c3b17557d438d7684bb6a4430da0d9
iammoses <inawo.moses@gmail.com>
2023-09-19 11:31:17 +0100

    delete modal added

---
+++ src/components/tables/TableBody.jsx
@@ -0,0 +1,119 @@
+import styled from "@emotion/styled";
+import { faEllipsisVertical, faTrashCan, faUserPen } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Switch } from "@mui/material";
+import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+
+const IOSSwitch = styled((props) => (
+    <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
+))(({ theme }) => ({
+    width: 42,
+    height: 26,
+    padding: 0,
+    '& .MuiSwitch-switchBase': {
+        padding: 0,
+        margin: 2,
+        transitionDuration: '300ms',
+        '&.Mui-checked': {
+            transform: 'translateX(16px)',
+            color: '#fff',
+            '& + .MuiSwitch-track': {
+                backgroundColor: theme.palette.mode === 'dark' ? '#2ECA45' : '#65C466',
+                opacity: 1,
+                border: 0,
+            },
+            '&.Mui-disabled + .MuiSwitch-track': {
+                opacity: 0.5,
+            },
+        },
+        '&.Mui-focusVisible .MuiSwitch-thumb': {
+            color: '#33cf4d',
+            border: '6px solid #fff',
+        },
+        '&.Mui-disabled .MuiSwitch-thumb': {
+            color:
+                theme.palette.mode === 'light'
+                    ? theme.palette.grey[100]
+                    : theme.palette.grey[600],
+        },
+        '&.Mui-disabled + .MuiSwitch-track': {
+            opacity: theme.palette.mode === 'light' ? 0.7 : 0.3,
+        },
+    },
+    '& .MuiSwitch-thumb': {
+        boxSizing: 'border-box',
+        width: 22,
+        height: 22,
+    },
+    '& .MuiSwitch-track': {
+        borderRadius: 26 / 2,
+        backgroundColor: theme.palette.mode === 'light' ? '#E9E9EA' : '#39393D',
+        opacity: 1,
+        transition: theme.transitions.create(['background-color'], {
+            duration: 500,
+        }),
+    },
+}));
+
+
+const TableBody = (props) => {
+
+     console.log(props)
+    const [option, setOption] = useState(false)
+   
+
+    const id = props?.id
+    const showModal = props?.handleOpen
+
+    const { pathname } = useLocation();
+    const path = pathname.split("/")[1]
+
+    const showData = (selected) => {
+        if (id === selected) {
+            setOption(!option)
+        }
+    }
+
+    const label = { inputProps: { 'aria-label': 'Size switch demo' } };
+
+    if (path === "assets") {
+        return (<tr>
+            <td>2 Bedroom Flat</td>
+            <td>4, oladele str, berger, Lagos...</td>
+            <td>Mr Bright</td>
+            <td>07054624163</td>
+            <td>NGN750,000</td>
+            <td className={option ? "showOption" : "hideOption"} > <FontAwesomeIcon icon={faEllipsisVertical} onClick={() => showData(id)} />
+                <div className={option ? "table-tooltip" : "hide-tooltip"}>
+                    <Link to="/assets/userinfo" className="d-flex align-items-center py-3 px-2"> <FontAwesomeIcon icon={faUserPen} style={{ color: '#3C91E6' }} /><p className="table-tooltip-info">View All Details</p> </Link>
+                    <div onClick={showModal} className="d-flex align-items-center py-2 px-2 --b-bottom --b-top"><FontAwesomeIcon icon={faTrashCan} style={{ color: '#CA1551' }} /> <p className="table-tooltip-delete">Delete Asset</p></div>
+                    <div className="d-flex align-items-center py-1 px-2">  <Switch {...label} color="primary" defaultChecked /> &nbsp; <p>Unlist Asset</p></div>
+                </div>
+            </td>
+        </tr>)
+    } else if (path === "users") {
+        return (<tr>
+            <td>Adekunle Williams</td>
+            <td>Adekunlewilliams@gmail.com</td>
+            <td>07054624163</td>
+            <td>5</td>
+            <td className={option ? "showOption" : "hideOption"} > <FontAwesomeIcon icon={faEllipsisVertical} onClick={() => showData(id)} />
+                <div className={option ? "table-tooltip" : "hide-tooltip"}>
+                    <Link to="/assets/userinfo" className="d-flex align-items-center py-3 px-2"> <FontAwesomeIcon icon={faUserPen} style={{ color: '#3C91E6' }} /><p className="table-tooltip-info">Profile</p> </Link>
+                    <div onClick={showModal} className="d-flex align-items-center py-2 px-2 --b-bottom --b-top"><FontAwesomeIcon icon={faTrashCan} style={{ color: '#CA1551' }} /> <p className="table-tooltip-delete">Delete</p></div>
+                    <div className="d-flex align-items-center py-1 px-2">  <Switch {...label} color="primary" defaultChecked /> &nbsp; <p>Deactivate</p></div>
+                </div>
+            </td>
+        </tr>)
+    } else if (path === "finance")
+        return (<tr>
+            <td>bronze</td>
+            <td>N3,000</td>
+            <td>4</td>
+            <td>3,056</td>
+            <td><FontAwesomeIcon icon={faEllipsisVertical} /></td>
+        </tr>)
+}
+
+export default TableBody
--- src/components/tables/Tables.jsx
+++ src/components/tables/Tables.jsx
@@ -2,12 +2,15 @@ import React, { useEffect, useState } from 'react'
 import "./Tables.scss";
 import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEllipsisVertical, faMagnifyingGlass, faUserPen } from '@fortawesome/free-solid-svg-icons'
+import { faCircleXmark, faEllipsisVertical, faFileCircleXmark, faMagnifyingGlass, faUserPen } from '@fortawesome/free-solid-svg-icons'
 import AssetsTableHead from '../../assets/data/AssetsTableHead';
 import { Link, useLocation } from 'react-router-dom';
 import UsersTableHead from '../../assets/data/UsersTableHead';
 import FinanceTableHead from '../../assets/data/FinanceTableHead';
 import viewProfile from '../../assets/images/asset-profile.png'
+import TableBody from './TableBody';
+import Trash from "../../assets/images/trash.png"
+import { Button, Modal } from 'react-bootstrap';
 
 const Tables = () => {
 
@@ -17,7 +20,6 @@ const Tables = () => {
     const getUser = async () => {
         try {
             const res = await axios.get("https://jsonplaceholder.typicode.com/users")
-            console.log(res.data)
             setData(res.data)
         } catch (error) {
             console.log(error)
@@ -27,11 +29,17 @@ const Tables = () => {
         getUser()
 
     }, [])
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
     const { pathname } = useLocation();
 
     const path = pathname.split("/")[1]
 
+    //   const [show, setShow] = useState(false);
 
+    //   const handleClose = () => setShow(false);
+    //   const handleShow = () => setShow(true);
 
     const headerRow = () => {
 
@@ -42,59 +50,85 @@ const Tables = () => {
         } else if (path === "finance")
             return FinanceTableHead
     }
-    const tableBody = () => {
 
-        if (path === "assets") {
-            return <tr>
-                <td>2 Bedroom Flat</td>
-                <td>4, oladele str, berger, Lagos...</td>
-                <td>Mr Bright</td>
-                <td>07054624163</td>
-                <td>NGN750,000</td>
-                <td><Link to="/assets/apartment"><FontAwesomeIcon icon={faEllipsisVertical} /></Link></td>
-            </tr>
-        } else if (path === "users") {
-            return <tr>
-                <td>Adekunle Williams</td>
-                <td>Adekunlewilliams@gmail.com</td>
-                <td>07054624163</td>
-                <td>5</td>
-                <td><Link to="/assets/userinfo"> <FontAwesomeIcon icon={faEllipsisVertical} /></Link></td>
-            </tr>
-        } else if (path === "finance")
-            return <tr>
-                <td>bronze</td>
-                <td>N3,000</td>
-                <td>4</td>
-                <td>3,056</td>
-                <td><FontAwesomeIcon icon={faEllipsisVertical} /></td>
-            </tr>
-    }
 
 
     const row = headerRow();
-    const body = tableBody()
+    const body = TableBody()
+
 
 
     return (
-        <div className='table'>
-            <table className=''>
-                <thead>
-                    <tr>
-                        {row.map((item) => <td>{item.name}</td>)}
-                        <td> <FontAwesomeIcon icon={faMagnifyingGlass} /></td>
-                    </tr>
+        <>
 
-                </thead>
-                <tbody>
-                    {data.length <= 0 ? (<p>Loading...</p>) : data.map((item) => {
-                        return body
-                    })
+            <div className='table'>
+                <table className=''>
+                    <thead>
+                        <tr>
+                            {row.map((item) => <td>{item.name}</td>)}
+                            <td> <FontAwesomeIcon icon={faMagnifyingGlass} /></td>
+                        </tr>
 
-                    }
-                </tbody>
-            </table>
-        </div>
+                    </thead>
+                    <tbody>
+                        {data.length <= 0 ? (<p>Loading...</p>) : data?.map((item, index) => {
+                            return <TableBody id={item?.id} handleOpen={handleOpen} />
+                        })
+
+                        }
+                    </tbody>
+                </table>
+            </div>
+
+            <Modal
+                // size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+                show={open}
+                onHide={handleClose}
+
+            >
+                <div className="modal-header py-4   pe-4">
+                    <button type="button" onClick={handleClose} className="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                    </button>
+                </div>
+
+                <div className='d-flex justify-content-center'>
+
+                    <div className='modal-container'>
+                        <img src={Trash} alt="trash" style={{
+                            width: 50,
+                            height: 50
+                        }} />
+                        <p style={{ color: '#273240', fontWeight: 600, fontSize: 18 }}>Delete Asset</p>
+                        <p style={{ color: '#021B33', fontSize: 14, marginTop: 4 }}>Are you sure you want to delete this Asset. This action cannot be undone once preformed.</p>
+                    </div>
+
+                </div>
+                <div style={{ backgroundColor: '#FAFAFA' }} className='d-flex justify-content-end --b-bottom py-3 px-2 gap-2 align-items-center '>
+                    <p style={{ border: '1px solid #ECECEC', padding: '5px 10px', borderRadius: 2 }}>Cancel</p>
+                    <p style={{ backgroundColor: '#CA1551', color: '#fff', borderRadius: 2, padding: '5px 10px' }}>Yes, Delete Asset</p>
+                </div>
+            </Modal>
+            {/* <Button variant="primary" onClick={handleShow}>
+                Launch demo modal
+            </Button>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal> */}
+        </>
     )
 }
 
